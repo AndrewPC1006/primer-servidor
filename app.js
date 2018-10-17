2545a3bdc86a9bf7695932abc1404bbe06b1a03f
Andres Posada <jposadac@uincca.edu.co>
2018-10-17 16:11:44 -0500

    Configuracion middleware

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const session = require('cookie-session');
 const port = process.env.PORT;
 const app = express();
 
+//Middleware
+//Información detallada en el terminal
+app.use(morgan('dev'));
+//Obtener los datos de las peticiones POST en el atributo body del request
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+//Configuración cookie-session
+app.use(
+    session({
+        secret:'node'
+    })
+)
 
 app.listen(port,function(){
     console.log('Escuchando en el puerto: ',port);
